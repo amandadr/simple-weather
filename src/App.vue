ab6769c3351678668396da5ba7bd30e56f0648b7
amandadr <amandadroy@gmail.com>
2024-02-28 15:06:03 -0400

    add dark-theme

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <div id="app">
+  <div id="app" :class="{ 'dark-theme': isDarkTheme }">
     <SearchForm @search="searchForWeather" />
+    <button @click="toggleTheme">Toggle Theme</button>
     <WeatherData ref="weatherData" :location="currentLocation" />
   </div>
 </template>
@@ -18,7 +19,22 @@ export default {
   data() {
     return {
       currentLocation: "Dartmouth",
+      isDarkTheme: false,
     };
+  },
+  mounted() {
+    // Fetch initial data on app load:
+    const initialLocation = "Dartmouth"; // Set a default location
+    this.searchForWeather(initialLocation);
+    // Set theme based on user preference or stored value
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    const storedTheme = localStorage.getItem("theme");
+    this.isDarkTheme =
+      storedTheme === "dark" || (storedTheme === null && prefersDark);
+    // Set initial styles immediately, before the next render
+    this.setThemeStyles();
   },
   methods: {
     searchForWeather(location) {
@@ -27,11 +43,16 @@ export default {
         this.$refs.weatherData.fetchWeatherData(location);
       });
     },
-  },
-  mounted() {
-    // Fetch initial data on app load:
-    const initialLocation = "Dartmouth"; // Set a default
-    this.searchForWeather(initialLocation);
+    toggleTheme() {
+      this.isDarkTheme = !this.isDarkTheme;
+      localStorage.setItem("theme", this.isDarkTheme ? "dark" : "light");
+      document.body.style.backgroundColor = this.isDarkTheme ? "#333" : "#fff";
+      document.body.style.color = this.isDarkTheme ? "#eee" : "#000";
+    },
+    setThemeStyles() {
+      document.body.style.backgroundColor = this.isDarkTheme ? "#333" : "#fff";
+      document.body.style.color = this.isDarkTheme ? "#eee" : "#000";
+    },
   },
 };
 </script>
